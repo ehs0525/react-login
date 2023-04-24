0ac127ffdfecbdc84311e690b588b28c20a53fde
엄희성 <ehs0525@naver.com>
2023-04-24 20:12:41 +0900

    Add: Auth Context 및 State 생성

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,15 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+
+import AuthContext from "./context/AuthProvider";
 
 const Login = () => {
+  const { setAuth } = useContext(AuthContext);
   const idRef = useRef();
   const errRef = useRef();
 
@@ -17,40 +26,75 @@ const Login = () => {
     setErrMsg("");
   }, [id, pw]);
 
-  return (
-    <section>
-      <p
-        ref={errRef}
-        className={errMsg ? "errmsg" : "offscreen"}
-        aria-live="assertive"
-      >
-        {errMsg}
-      </p>
-      <h1>로그인</h1>
-      <form>
-        {/* 아이디 */}
-        <label htmlFor="username">아이디</label>
-        <input
-          type="text"
-          id="username"
-          ref={idRef}
-          autoComplete="off"
-          onChange={(e) => setId(e.target.value)}
-          value={id}
-          required
-        />
+  const handleSubmit = useCallback(
+    async (e) => {
+      e.preventDefault();
+      console.log(id, pw);
+      setId("");
+      setPw("");
+      setSuccess(true);
+    },
+    [id, pw]
+  );
 
-        {/* 비밀번호 */}
-        <label htmlFor="password">비밀번호</label>
-        <input
-          type="password"
-          id="password"
-          onChange={(e) => setPw(e.target.value)}
-          value={pw}
-          required
-        />
-      </form>
-    </section>
+  return (
+    <>
+      {success ? (
+        <section>
+          <h1>로그인 성공</h1>
+          <br />
+          <p>
+            <a href="#">홈으로 가기</a>
+          </p>
+        </section>
+      ) : (
+        <section>
+          <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
+          <h1>로그인</h1>
+          <form onSubmit={handleSubmit}>
+            {/* 아이디 */}
+            <label htmlFor="username">아이디</label>
+            <input
+              type="text"
+              id="username"
+              ref={idRef}
+              autoComplete="off"
+              onChange={(e) => setId(e.target.value)}
+              value={id}
+              required
+            />
+
+            {/* 비밀번호 */}
+            <label htmlFor="password">비밀번호</label>
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => setPw(e.target.value)}
+              value={pw}
+              required
+            />
+
+            {/* 로그인 버튼 */}
+            <button>로그인</button>
+          </form>
+
+          <p>
+            계정이 필요한가요?
+            <br />
+            <span className="line">
+              {/*put router link here*/}
+              <a href="#">회원가입</a>
+            </span>
+          </p>
+        </section>
+      )}
+    </>
   );
 };
 
