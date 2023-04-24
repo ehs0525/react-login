0ec551adae6587ba279030434fa9df16ecd423a3
엄희성 <ehs0525@naver.com>
2023-04-24 19:46:54 +0900

    Add: Login id input

---
+++ src/Login.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const Login = () => {
+  const idRef = useRef();
+  const errRef = useRef();
+
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    idRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [id, pw]);
+
+  return (
+    <section>
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+      <h1>로그인</h1>
+      <form>
+        {/* 아이디 */}
+        <label htmlFor="username">아이디</label>
+        <input
+          type="text"
+          id="username"
+          ref={idRef}
+          autoComplete="off"
+          onChange={(e) => setId(e.target.value)}
+          value={id}
+          required
+        />
+      </form>
+    </section>
+  );
+};
+
+export default Login;
